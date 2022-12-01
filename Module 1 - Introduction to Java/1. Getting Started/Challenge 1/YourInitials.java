8739a17d973041b8a76802a6500644aa3ad38afe
zenocodes <azronebrian@gmail.com>
2022-12-01 19:41:07 +0300

    done challenge

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,14 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link)
+
+         System.out.println("  A    BBBB    OOO ");
+         System.out.println(" A A   B   B  O   O");
+         System.out.println("A   A  B   B  O   O");
+         System.out.println("AAAAA  BBBBB  O   O");
+         System.out.println("A   A  B   B  O   O");
+         System.out.println("A   A  B   B  O   O");
+         System.out.println("A   A  BBBB    OOO ");
+         
     }
 }
--- Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.2/Biography.java
@@ -5,18 +5,26 @@ public class Biography {
 
 
         // name (text)
+        String name = "Azron";
         // age (whole number)
+        int age = 28;
         // country (text)
+        String country = "Kenya";
         // sport (text)
+        String sport = "football";
         // hours (whole number)
+        int hours = 1;
         // game (text)
+        String game = "Blockly";
         // subject (text)
+        String subject = "Math";
         // grade (character)
+        char grade = 'A';
      
-        System.out.println("My name is <name>. I'm <age> years old, and I'm from <country> ");
-        System.out.println("My favourite sport is <sport>. I play for <hours> hours a day");
-        System.out.println("When I'm tired, I like to play <game>.");
-        System.out.println("In school, my favourite subject was <subject>, I scored a <grade>.");
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country);
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject +", I scored a " + grade + ".");
 
         // Compare your result to what's on Learn the Part.
     }
