fd37447ce4754ea03daf02e21483f111682495de
zenocodes <azronebrian@gmail.com>
2022-12-02 11:31:00 +0300

    challenge done

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -11,27 +11,32 @@ public class SignUp {
         System.out.println("Welcome to JavaGram! Let's sign you up.");
         
         System.out.println("What is your first name?");
-        
+        String firstName = scan.nextLine();
         System.out.println("What is your last name?");
-
+        String lastName = scan.nextLine();
         System.out.println("How old are you?");
+        int age = scan.nextInt();
 
         System.out.println("Make a username");  
         // If you experience trouble picking up the username, please carefully read the instructions on Learn the Part.
+        String userName = scan.nextLine();
         
         System.out.println("What city do you live in?");
+        String city = scan.nextLine();
 
         System.out.println("What country is that?");
+        String country = scan.nextLine();
         
         System.out.println("Thank you for joining JavaGram!");
         System.out.println("\nHere is the information you entered:");
-        System.out.println("\tFirst Name: <firstName>");
-        System.out.println("\tLast Name: <lastName>");
-        System.out.println("\tAge: <age>");
-        System.out.println("\tUsername: <userName>");
-        System.out.println("\tCity: <city>");
-        System.out.println("\tCountry: <country>");
+        System.out.println("\tFirst Name: " + firstName);
+        System.out.println("\tLast Name: " + lastName);
+        System.out.println("\tAge: " + age);
+        System.out.println("\tUsername: " + userName);
+        System.out.println("\tCity: " + city);
+        System.out.println("\tCountry: " + country);
 
         //close scanner. It's good practice :D ! 
+        scan.close();
     }
 }
